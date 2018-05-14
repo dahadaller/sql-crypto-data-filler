ae128fabe48978ec7031b9614de6b0e5fb029631
David Hadaller <david.hadaller@macaulay.cuny.edu>
2018-05-14 01:08:33 -0400

    Added more time for js to execute in startup script.

--- startup.sh
+++ startup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 cd /media/david/Drive/Binder/Csc_336/Assignments/Group\ Project/Code/sql-crypto-data-filler
+echo first step executing
 mysql -u root -p23058319 < firstStep.sql
 npm start &
-sleep 3
+sleep 10
+echo last step executing
 mysql -u root -p23058319 < lastStep.sql
